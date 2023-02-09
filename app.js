6c609b4afd229135fc68405f6aea76acd181cdec
Ezar_Ch.S <ezar.chandra@gmail.com>
2023-02-09 18:20:15 +0700

    sudah terhubung dengan tampilan awal

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const Controller = require('./controllers/controller')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', Controller.home)
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is now working on port ${port}...`)
+})
---
+++ controllers/controller.js
@@ -0,0 +1,10 @@
+
+
+class Controller {
+    static home(req, res){
+        res.render('home')
+    }
+
+
+}
+module.exports = Controller
